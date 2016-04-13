517f9a45d5b6033c369060c932209bb0d2ba69c2
yuki-hirako <yuki.hirako@dena.com>
2016-04-13 11:44:09 +0900

    create frexible retry

--- flexible_retry.gemspec
+++ flexible_retry.gemspec
@@ -9,14 +9,14 @@ Gem::Specification.new do |spec|
   spec.authors       = ["kocchi"]
   spec.email         = ["y.hirako63@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
+  spec.summary       = ""
+  spec.description   = ""
   spec.homepage      = "TODO: Put your gem's website or public repo URL here."
 
   # Prevent pushing this gem to RubyGems.org by setting 'allowed_push_host', or
   # delete this section to allow pushing this gem to any host.
   if spec.respond_to?(:metadata)
-    spec.metadata['allowed_push_host'] = "TODO: Set to 'http://mygemserver.com'"
+    spec.metadata['allowed_push_host'] = ""
   else
     raise "RubyGems 2.0 or newer is required to protect against public gem pushes."
   end
--- lib/flexible_retry.rb
+++ lib/flexible_retry.rb
@@ -1,5 +1,41 @@
 require "flexible_retry/version"
 
 module FlexibleRetry
-  # Your code goes here...
+  def retry_if(options = {}, &block)
+    RetryProxy.new(self, options.merge(expected_logical_value: false), &block)
+  end
+
+  def retry_unless(options = {}, &block)
+    RetryProxy.new(self, options.merge(expected_logical_value: true), &block)
+  end
+
+  class RetryProxy
+    def initialize(subject,  expected_logical_value: false,retry_count: 5, interval: 1, &block)
+      @expected_logical_value = expected_logical_value
+      @subject                = subject
+      @interval               = interval
+      @retry_count            = retry_count
+      @checker                = block
+    end
+
+    def method_missing(name, *args, &block)
+      (1 + @retry_count).times do |i|
+        result = @subject.send(name, *args, &block)
+
+        to_retry = begin
+                     @expected_logical_value ^ @checker.call(result)
+                   rescue => e
+                     true
+                   end
+
+        if to_retry && i < @retry_count
+          warn "retry: #{i+1}" if ENV["DEBUG"] == "1"
+          sleep @interval
+          next
+        else
+          return result
+        end
+      end
+    end
+  end
 end
